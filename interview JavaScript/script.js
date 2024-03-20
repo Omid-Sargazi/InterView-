44f3ef730fb1b1ed7cba6434a99fd062ced9a574
OmidSargazi <om.sargazi@gmail.com>
2024-03-21 01:43:52 +0330

    spread operators

--- interview JavaScript/script.js
+++ interview JavaScript/script.js
@@ -12,3 +12,32 @@ console.log(add(4, 3));
 function add(a, b) {
   return a + b;
 }
+// closure
+
+function createCounter() {
+  let value = 0;
+  function incr() {
+    return ++value;
+  }
+  function dec() {
+    return --value;
+  }
+  return {
+    incr: incr,
+    dec: dec,
+  };
+}
+
+const counter1 = createCounter();
+const counter2 = createCounter();
+const counter3 = createCounter();
+console.log(counter1.dec(), "counter1");
+console.log(counter2.incr(), "counter2");
+console.log(counter2.incr(), "counter2");
+console.log(counter2.incr(), "counter2");
+console.log(counter3.incr(), "counter3");
+
+// spread operators
+let aa = [1, 2, 3];
+let bb = ["a", "b"];
+console.log([...aa, ...bb]);
