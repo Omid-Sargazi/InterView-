1325e6711b901a4d6c4213c8d426d5e859e9aa7d
OmidSargazi <om.sargazi@gmail.com>
2024-04-07 23:00:32 +0330

    this object in javascript

--- AdvancedJavascript/main.js
+++ AdvancedJavascript/main.js
@@ -1,48 +1,72 @@
-// let a = 10;
-// function outer() {
-//   let b = 20;
+// // let a = 10;
+// // function outer() {
+// //   let b = 20;
+// //   function inner() {
+// //     let c = 30;
+// //     console.log(a, b, c);
+// //   }
+// //   inner();
+// // }
+// // outer();
+
+// // Closures
+// function outerClosure() {
+//   let counter = 0;
 //   function inner() {
-//     let c = 30;
-//     console.log(a, b, c);
+//     counter++;
+//     console.log(counter);
 //   }
-//   inner();
+//   return inner;
 // }
-// outer();
+// const fn = outerClosure();
+// fn();
+// fn();
 
-// Closures
-function outerClosure() {
-  let counter = 0;
-  function inner() {
-    counter++;
-    console.log(counter);
-  }
-  return inner;
+// ////////////////
+// function sum(a, b, c) {
+//   return a + b + c;
+// }
+
+// console.log(sum(1, 2, 3));
+
+// function curry(fn) {
+//   return function (a) {
+//     return function (b) {
+//       return function (c) {
+//         return fn(a, b, c);
+//       };
+//     };
+//   };
+// }
+
+// const curriedSum = curry(sum);
+// console.log(curriedSum(1)(2)(3));
+
+// const add1 = curriedSum(1);
+// const add2 = add1(2);
+// const add3 = add2(3);
+// console.log(add3);
+
+// //Implicit binding
+
+// const person = {
+//   name: "Omid",
+//   sayMyName: function () {
+//     console.log(`My name is ${this.name}`);
+//   },
+// };
+// person.sayMyName();
+
+// function sayMyName() {
+//   console.log(`My name is ${this.name}`);
+// }
+// sayMyName.call(person);
+
+// // ///////////////////This/////////////////////////
+globalThis.name = "Omid";
+
+function hello() {
+  return globalThis;
 }
-const fn = outerClosure();
-fn();
-fn();
 
-////////////////
-function sum(a, b, c) {
-  return a + b + c;
-}
-
-console.log(sum(1, 2, 3));
-
-function curry(fn) {
-  return function (a) {
-    return function (b) {
-      return function (c) {
-        return fn(a, b, c);
-      };
-    };
-  };
-}
-
-const curriedSum = curry(sum);
-console.log(curriedSum(1)(2)(3));
-
-const add1 = curriedSum(1);
-const add2 = add1(2);
-const add3 = add2(3);
-console.log(add3);
+console.log(hello());
--- interview JavaScript/script.js
+++ interview JavaScript/script.js
@@ -1,43 +1,80 @@
-const createHelloWOrld = function () {
-  return function (...args) {
-    return "Hello World ";
-  };
+// const createHelloWOrld = function () {
+//   return function (...args) {
+//     return "Hello World ";
+//   };
+// };
+// const a = createHelloWOrld();
+// console.log(a());
+// // we have Function hosting in javascript
+// // we don't have hosting in arrow function
+// console.log(add(4, 3));
+
+// function add(a, b) {
+//   return a + b;
+// }
+// // closure
+
+// function createCounter() {
+//   let value = 0;
+//   function incr() {
+//     return ++value;
+//   }
+//   function dec() {
+//     return --value;
+//   }
+//   return {
+//     incr: incr,
+//     dec: dec,
+//   };
+// }
+
+// const counter1 = createCounter();
+// const counter2 = createCounter();
+// const counter3 = createCounter();
+// console.log(counter1.dec(), "counter1");
+// console.log(counter2.incr(), "counter2");
+// console.log(counter2.incr(), "counter2");
+// console.log(counter2.incr(), "counter2");
+// console.log(counter3.incr(), "counter3");
+
+// // spread operators
+// let aa = [1, 2, 3];
+// let bb = ["a", "b"];
+// console.log([...aa, ...bb]);
+var obj = {
+  name: "Seid",
+  Hi: hello,
 };
-const a = createHelloWOrld();
-console.log(a());
-// we have Function hosting in javascript
-// we don't have hosting in arrow function
-console.log(add(4, 3));
 
-function add(a, b) {
-  return a + b;
-}
-// closure
+var name = "John";
 
-function createCounter() {
-  let value = 0;
-  function incr() {
-    return ++value;
-  }
-  function dec() {
-    return --value;
-  }
-  return {
-    incr: incr,
-    dec: dec,
-  };
+function hello() {
+  console.log(this.name);
 }
 
-const counter1 = createCounter();
-const counter2 = createCounter();
-const counter3 = createCounter();
-console.log(counter1.dec(), "counter1");
-console.log(counter2.incr(), "counter2");
-console.log(counter2.incr(), "counter2");
-console.log(counter2.incr(), "counter2");
-console.log(counter3.incr(), "counter3");
+obj.Hi();
+var helloJohn = obj.Hi;
 
-// spread operators
-let aa = [1, 2, 3];
-let bb = ["a", "b"];
-console.log([...aa, ...bb]);
+helloJohn();
+///////////////this in arrow functions/////////////////
+console.log("object");
+const Person = {
+  name: "Emily",
+  print: () => {
+    console.log(this.name);
+  },
+};
+
+Person.print();
+
+var obj3 = {
+  name: "Mika",
+};
+function w2() {
+  console.log(this.name, "w2");
+}
+
+// w2.apply(obj3);
+w2.call(obj3);
+
+// w2();
