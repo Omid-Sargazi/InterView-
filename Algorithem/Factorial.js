43e77336c61408e6dd01aad95b8430fa2e975cc4
OmidSargazi <om.sargazi@gmail.com>
2024-04-15 01:55:22 +0330

    Factorial of a Number1

---
+++ Algorithem/Factorial.js
@@ -0,0 +1,15 @@
+console.log("Factorial");
+function factorial(n) {
+  if (n < 0) {
+    return null;
+  }
+  if (n == 0 || n == 1) {
+    return 1;
+  }
+  let f = 1;
+  for (let i = 1; i <= n; i++) {
+    f = f * i;
+  }
+  return f;
+}
+console.log(factorial(50));
