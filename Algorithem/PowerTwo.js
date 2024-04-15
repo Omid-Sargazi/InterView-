68a8319c881d99669dd60b8c516526144a837457
OmidSargazi <om.sargazi@gmail.com>
2024-04-16 03:13:52 +0330

    Power of two bit wise

--- Algorithem/PowerTwo.js
+++ Algorithem/PowerTwo.js
@@ -17,3 +17,15 @@ console.log(isPowerOfTwo(1024));
 console.log(isPowerOfTwo(8));
 
 // Big-O is log(n)
+
+function isPowerOfTwoBitWise(n) {
+  if (n < 1) {
+    return false;
+  }
+  return (n & (n - 1)) === 0;
+}
+console.log("Power of two ==========================");
+console.log(isPowerOfTwoBitWise(14));
+console.log(isPowerOfTwoBitWise(140));
+console.log(isPowerOfTwoBitWise(1024));
+console.log(isPowerOfTwoBitWise(8));
