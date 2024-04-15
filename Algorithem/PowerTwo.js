f496005399c4872bcbfd5c07f25f174a0683f22c
OmidSargazi <om.sargazi@gmail.com>
2024-04-16 03:08:26 +0330

    Power of Two

---
+++ Algorithem/PowerTwo.js
@@ -0,0 +1,19 @@
+function isPowerOfTwo(n) {
+  if (n < 1) {
+    return false;
+  }
+  while (n > 1) {
+    if (n % 2 !== 0) {
+      return false;
+    }
+    n = n / 2;
+  }
+  return true;
+}
+
+console.log(isPowerOfTwo(14));
+console.log(isPowerOfTwo(140));
+console.log(isPowerOfTwo(1024));
+console.log(isPowerOfTwo(8));
+
+// Big-O is log(n)
