4e45ebfc6a967fb284f0d2693284e3d190b5c33f
OmidSargazi <om.sargazi@gmail.com>
2024-04-19 20:35:01 +0330

    Factorial Recursion

--- Algorithem/Recursion.js
+++ Algorithem/Recursion.js
@@ -3,12 +3,20 @@ console.log("Recursion");
 //0,1,1,2,3,5,8,13,21,....
 
 function fiboRecu(n) {
-  if (n == 0 || n == 1) {
+  if (n == 1 || n == 2) {
     return 1;
   }
-  while (n >= 1) {
+  while (n >= 0) {
     return fiboRecu(n - 1) + fiboRecu(n - 2);
     n--;
   }
 }
-console.log(fiboRecu(8));
+console.log(fiboRecu(6));
+
+//
+function recursiveFibo(n) {
+  if (n < 2) return n;
+  return recursiveFibo(n - 1) + recursiveFibo(n - 2);
+}
+
+console.log(recursiveFibo(6));
