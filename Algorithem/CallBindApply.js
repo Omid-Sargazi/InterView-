def16264b70c41f9040e34e7dc46e8c136b1ddf4
OmidSargazi <om.sargazi@gmail.com>
2024-04-20 23:59:12 +0330

    Apply method in javascript

--- Algorithem/CallBindApply.js
+++ Algorithem/CallBindApply.js
@@ -22,3 +22,29 @@ console.log(user.name.call(SaaedSargazi));
 console.log(user.name.call(VahidSargazi));
 
 console.log(user.hobbies.call(VahidSargazi, "football", "climbing"));
+
+//////////////////////////////////////Apply Method//////////////////////////
+
+const UseWithApply = {
+  name() {
+    return `${this.firstName} ${this.lastName}`;
+  },
+  hobbies(one, two, three) {
+    return `My hobbies are ${one} ${two} ${three}`;
+  },
+};
+
+const Omid = {
+  firstName: "Omid",
+  lastName: "Sargazi",
+};
+const David = {
+  firstName: "David",
+  lastName: "Sargazi",
+};
+
+console.log(UseWithApply.name.apply(Omid), "Apply");
+console.log(
+  UseWithApply.hobbies.apply(VahidSargazi, ["PS5", "football", "body"])
+);
+console.log(UseWithApply.name.apply(David), "Apply");
