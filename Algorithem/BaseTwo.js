0da279b76efc10f3a87c0c5030ca8496022e5d9e
OmidSargazi <om.sargazi@gmail.com>
2024-04-21 01:51:43 +0330

    Two Base to 10 base

--- Algorithem/BaseTwo.js
+++ Algorithem/BaseTwo.js
@@ -12,4 +12,20 @@ function baseTwo(n) {
   console.log(s);
 }
 
-baseTwo(8);
+// baseTwo(8);
+
+////////////////////////////////////////Base Two to Ten/////////////////////
+
+function TwoTenBase(n) {
+  let p = 1;
+  let s = 0;
+  while (n > 0) {
+    s = s + (n % 10) * p;
+    console.log(s);
+    p = p * 2;
+    n = ~~(n / 10);
+  }
+  console.log(s, "TwoTenBase");
+}
+
+TwoTenBase(11111111110);
