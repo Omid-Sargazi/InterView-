f77dec1ba806f53336d4ec5bd839251735f19372
OmidSargazi <om.sargazi@gmail.com>
2024-04-21 22:10:01 +0330

    Recursive Binary Search

--- Algorithem/SearchAlgorithms.js
+++ Algorithem/SearchAlgorithms.js
@@ -31,11 +31,52 @@ function BinarySearch(array, t = 0) {
   }
   return "not found";
 }
-console.log(BinarySearch([1, 2, 3, 4, 5, 6, 7], 70));
+console.log(BinarySearch([1, 2, 3, 4, 5, 6, 7], 7));
 
-// let arr = [1, 2, 3, 4, 5, 6, 7];
-// let n = arr.length;
-// // arr = arr.slice(0, 4);
-// console.log(arr[~~(n / 2)], "end");
+console.log("///////////BinarySearchWithoutRecursive/////////");
 
-// Big-O(log(n))
+function BinarySearchWithoutRecursive(array, n) {
+  let left = 0;
+  let right = array.length - 1;
+
+  while (left <= right) {
+    let middle = Math.floor((left + right) / 2);
+
+    if (n === array[middle]) {
+      return `${middle} ${array[middle]}`;
+    }
+
+    if (n < array[middle]) {
+      right = middle - 1;
+    } else {
+      left = middle + 1;
+    }
+  }
+  return "not found";
+}
+console.log(
+  BinarySearchWithoutRecursive([10, 20, 30, 40, 100, 500, 600, 700], 700)
+);
+
+//////////////////////////Recursive Binary Search////////////////////
+function recursiveBinarySearch(array, target) {
+  return search(array, target, 0, array.length - 1);
+}
+function search(array, target, left, right) {
+  if (left > right) {
+    return -1;
+  }
+  let middle = Math.floor((left + right) / 2);
+  if (target === array[middle]) {
+    return middle;
+  }
+
+  if (target < array[middle]) {
+    return search(array, target, left, middle - 1);
+  } else {
+    return search(array, target, middle + 1, right);
+  }
+}
+
+let array = [1, 2, 4, 6, 8, 9, 10, 14, 15, 16, 160, 170];
+console.log(recursiveBinarySearch(array, 160));
