948c809af68b8df75305febd8993223b8c47e679
OmidSargazi <om.sargazi@gmail.com>
2024-04-21 03:38:58 +0330

    Transform base two into eight base

--- Algorithem/BaseTwo.js
+++ Algorithem/BaseTwo.js
@@ -29,3 +29,25 @@ function TwoTenBase(n) {
 }
 
 TwoTenBase(11111111110);
+
+/////////////////////////////TwoEight/////////////////////
+////////////1111111==>
+console.log("TwoToEightBase");
+console.log(~~(111110111 / 1000));
+
+function TwoToEightBase(n) {
+  let p = 1;
+  let s = 0;
+  let r = 0;
+  let q = 0;
+  while (n > 0) {
+    r = n % 1000;
+    q = (~~(r / 10) % 10) * 4 + (~~(r / 10) % 10) * 2 + (r % 10);
+    s = s + q * p;
+    p = p * 10;
+    n = ~~(n / 1000);
+  }
+  console.log(s);
+}
+
+TwoToEightBase(111001001111);
